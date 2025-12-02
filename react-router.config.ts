1e11ceb04d428d980cffc04281d5b0e1e3f88674
Althea <altheagene@users.noreply.github.com>
2025-12-02 21:43:24 +0800

    commit maintsx

--- react-router.config.ts
+++ react-router.config.ts
@@ -6,4 +6,12 @@ export default {
   ssr: false,
 } satisfies Config;
 
+// import type { Config } from "@react-router/dev/config";
+
+// export default {
+//   ssr: false,       // SPA mode
+//   router: {
+//     type: "hash",   // Use hash routing
+//   },
+// } satisfies Config;
 
